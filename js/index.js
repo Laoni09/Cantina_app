e0b5bdb4187fc615488b25cd2fa995cf517b2d6c
Laoni09 <laonimoreira.capanema@gmail.com>
2022-09-13 23:24:02 -0300

    att 13/09

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
 var valor = 0
+function subtrairpao(){
+    valor -= 3.50
+    document.querySelector('.resultado').innerHTML = valor
+}
+
 function somarpao(){
     valor += 3.50
     document.querySelector('.resultado').innerHTML = valor
 }
+
+function subtrairtoddy(){
+    valor -= 2.50
+    document.querySelector('.resultado').innerHTML = valor
+}
+
 function somartoddy(){
     valor += 2.50
     document.querySelector('.resultado').innerHTML = valor
 }
+
+function subtrairtrento(){
+    valor -= 1.50
+    document.querySelector('.resultado').innerHTML = valor
+}
+
 function somartrento(){
     valor += 1.50
     document.querySelector('.resultado').innerHTML = valor
 }
+
 function reset(){
     valor = 0
     document.querySelector('.resultado').innerHTML = valor
