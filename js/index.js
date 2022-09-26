9d423672cc56a34ece3eba1db763f67ecd9bc270
Laoni09 <laonimoreira.capanema@gmail.com>
2022-09-26 14:33:09 -0300

    att. 26/09

--- js/index.js
+++ js/index.js
@@ -2,34 +2,41 @@ var valor = 0
 function subtrairpao(){
     valor -= 3.50
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML -= 'Pão de queijo------------------------R$3,50'
 }
 
 function somarpao(){
     valor += 3.50
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML += 'Pão de queijo------------------------R$3,50'
 }
 
 function subtrairtoddy(){
     valor -= 2.50
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML -= 'Toddynho------------------------R$2,50'
 }
 
 function somartoddy(){
     valor += 2.50
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML += 'Toddynho------------------------R$2,50'
 }
 
 function subtrairtrento(){
     valor -= 1.50
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML -= 'Trento------------------------R$1,50'
 }
 
 function somartrento(){
     valor += 1.50
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML += 'Trento------------------------R$1,50'
 }
 
 function reset(){
     valor = 0
     document.querySelector('.resultado').innerHTML = valor
+    document.querySelector('.nota').innerHTML += ''
 }
