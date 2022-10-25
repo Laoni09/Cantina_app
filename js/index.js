0d6b04ceee7c2481237374b42744c201dd536c17
Laoni09 <laonimoreira.capanema@gmail.com>
2022-10-25 19:51:52 -0300

    att 25/10

--- js/index.js
+++ js/index.js
@@ -2,25 +2,29 @@ var valor = 0
 var paes = 0
 var toddys = 0
 var trentos = 0
+var cliquespao = 0
+var cliquestrento = 0
+var cliquestoddy = 0
 function subtrairpao(){
-    if (paes > 0){
+    if (paes >= 1){
         paes -= 1
+        document.getElementById('pao_de_queijo').innerHTML = ''
     }
-    if (valor >= 3.50 && paes > 0){
+    if (valor >= 3.50 && paes >= 1){
         valor -= 3.50
         document.querySelector('.resultado').innerHTML = valor
-    }if (valor >= 3.50 && paes < 1){
-        document.querySelector('.resultado').innerHTML = valor
-    }else{
-        valor = 0
-        document.querySelector('.resultado').innerHTML = valor
-    }
-    if (paes >= 1) {
-        document.getElementById('pao_de_queijo').innerHTML = ''
         document.getElementById('pao_de_queijo').innerHTML = `PÃO DE QUEIJO ---------- ${paes}x`
-    }else{
+    }
+    if (valor > 3.50 && paes < 1){
         document.getElementById('pao_de_queijo').innerHTML = ''
-        valor -= 3.50
+        if (cliquespao >= 0){
+            valor -= 3.50
+            cliquespao -= 1
+        }
+        document.querySelector('.resultado').innerHTML = valor
+    }
+    if (valor <= 3.50 && paes < 1) {
+        valor = 0
         document.querySelector('.resultado').innerHTML = valor
     }
 }
@@ -31,26 +35,30 @@ function somarpao(){
     paes += 1
     document.getElementById('pao_de_queijo').innerHTML = ''
     document.getElementById('pao_de_queijo').innerHTML = `PÃO DE QUEIJO ---------- ${paes}x`
+    cliquespao += 1
 }
 
 function subtrairtoddy(){
-    if (toddys > 0){
+    if (toddys >= 1){
         toddys -= 1
     }
-    if (valor >= 2.50 && toddys > 0){
+    if (valor >= 2.50 && toddys >= 1){
         valor -= 2.50
         document.querySelector('.resultado').innerHTML = valor
-    }if (valor >= 2.50 && toddys < 1){
-        document.querySelector('.resultado').innerHTML = valor
-    }else{
-        valor = 0
-        document.querySelector('.resultado').innerHTML = valor
-    }
-    if (toddys >= 1) {
         document.getElementById('toddynho').innerHTML = ''
         document.getElementById('toddynho').innerHTML = `TODDYNHO ---------- ${toddys}x`
-    }else{
+    }
+    if (valor >= 2.50 && toddys < 1){
         document.getElementById('toddynho').innerHTML = ''
+        if (cliquestoddy >= 0){
+            valor -= 2.50
+            cliquestoddy -= 1
+        }
+        document.querySelector('.resultado').innerHTML = valor
+    }
+    if (valor < 2.50 && toddys < 1) {
+        valor = 0
+        document.querySelector('.resultado').innerHTML = valor
     }
 }
 
@@ -60,26 +68,30 @@ function somartoddy(){
     toddys += 1
     document.getElementById('toddynho').innerHTML = ''
     document.getElementById('toddynho').innerHTML = `TODDYNHO ---------- ${toddys}x`
+    cliquestoddy += 1
 }
 
 function subtrairtrento(){
-    if (trentos > 0){
+    if (trentos >= 1){
         trentos -= 1
     }
-    if (valor >= 1.50 && trentos > 0){
+    if (valor >= 1.50 && trentos >= 1){
         valor -= 1.50
         document.querySelector('.resultado').innerHTML = valor
-    }if (valor >= 1.50 && trentos < 1){
-        document.querySelector('.resultado').innerHTML = valor
-    }else{
-        valor = 0
-        document.querySelector('.resultado').innerHTML = valor
-    }
-    if (trentos >= 1) {
         document.getElementById('trento').innerHTML = ''
         document.getElementById('trento').innerHTML = `TRENTO ---------- ${trentos}x`
-    }else{
+    }
+    if (valor >= 1.50 && trentos < 1){
         document.getElementById('trento').innerHTML = ''
+        if (cliquestrento >= 0){
+            valor -= 1.50
+            cliquestrento -= 1
+        }
+        document.querySelector('.resultado').innerHTML = valor
+    }
+    if (valor < 1.50 && trentos < 1) {
+        valor = 0
+        document.querySelector('.resultado').innerHTML = valor
     }
 }
 
@@ -89,6 +101,7 @@ function somartrento(){
     trentos += 1
     document.getElementById('trento').innerHTML = ''
     document.getElementById('trento').innerHTML = `TRENTO ---------- ${trentos}x`
+    cliquestrento += 1
 }
 
 function reset(){
@@ -96,8 +109,11 @@ function reset(){
     document.querySelector('.resultado').innerHTML = valor
     document.getElementById('trento').innerHTML = ''
     trentos = 0
+    cliquestrento = 0
     document.getElementById('toddynho').innerHTML = ''
     toddys = 0
+    cliquestoddy = 0
     document.getElementById('pao_de_queijo').innerHTML = ''
     paes = 0
+    cliquespao = 0
 }
